bb3b181449af4efd543da783efcbca59311905f2
runbutescape <95318563+runbutescape@users.noreply.github.com>
2021-12-07 10:21:14 -0700

    Add files via upload
    Annual global sales per month

---
+++ global_sales_time.py
@@ -0,0 +1,30 @@
+# numpy is needed for x-tick label positions
+import matplotlib.pyplot as plt
+import pandas as pd, numpy as np
+import sys
+
+def main():
+
+    ### This is a local path for my computer, change if vgsales.csv is located elsewhere
+    data = pd.read_csv("C:\\Users\\LANBox\\Desktop\\ISTA_131\\vgsales.csv")
+    ###
+    df = pd.DataFrame(data)
+    df.sort_values(by=["Year"], inplace=True, ascending=False)
+
+    result = pd.DataFrame(data = df['NA_Sales'], index = df["Year"])
+    plt.plot(result.index, result['NA_Sales'], color = 'r', label = "NA Sales")
+
+    result = pd.DataFrame(data = df['EU_Sales'], index = df["Year"])
+    plt.plot(result.index, result['EU_Sales'], color = 'g', label = "EU Sales")
+
+    result = pd.DataFrame(data = df['JP_Sales'], index = df["Year"])
+    plt.plot(result.index, result['JP_Sales'], color = 'b', label = "JP Sales")
+
+    plt.title("Regional Video Game Sales Over Time")
+    plt.xlabel("Year")
+    plt.ylabel("Sales per month (millions)")
+    plt.legend(loc = 'upper left')
+
+    plt.show()
+
+main()
